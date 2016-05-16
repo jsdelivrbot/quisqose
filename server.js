05ace2f8f4a18f410473b9a936c555a412d8ae9f
Kevin <katokh89@gmail.com>
2016-05-16 10:35:42 -0700

    update server.js for heroku.

--- server.js
+++ server.js
@@ -2,11 +2,23 @@ var express = require('express');
 
 //Create our app
 var app = express();
+//allows heroku to run the app
+const PORT = process.env.PORT || 3000;
+//For the openweathermap api. heroku cannot run on HTTP, only HTTPS.
+//This code will direct traffic from HTTPS to HTTP.
+//Express Middleware
+app.use(function (req, res, next){
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
 
 //use the public folder
 app.use(express.static('public'));
 
 //start the server
-app.listen(3000, function(){
-    console.log('Express server is up on port 3000');
+app.listen(PORT, function(){
+    console.log('Express server is up on port ' + PORT);
 });
