960f244f2a754d69132ff556384855b33d90776f
Kevin <katokh89@gmail.com>
2016-06-08 14:15:46 -0700

    add links to Home.jsx

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -13,6 +13,12 @@ var Home = React.createClass({
         <Link to='portfolio'><img className="text-center" src={"img/eeyore.jpeg"}/></Link>
         <h3>Travelbug</h3>
         <Link to='travelbug'><img className="text-center" src={"img/travelbugthumb1.jpeg"}/></Link>
+        <h3>pathHop</h3>
+        <Link to='pathhop'><img className="text-center" src={"img/comealongthumb1.jpeg"}/></Link>
+        <h3>React Weather</h3>
+        <Link to='weather'><img className="text-center" src={"img/comealongthumb1.jpeg"}/></Link>
+        <h3>Circles and Squares of Doom</h3>
+        <Link to='circles'><img className="text-center" src={"img/doomthumb1.jpeg"}/></Link>
       </div>
     )
   }
